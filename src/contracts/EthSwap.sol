3e18a307f90c4d8aa6a832a2a327aa70215fcf95
nahuelontech <87518314+nahuelontech@users.noreply.github.com>
2021-09-09 15:36:59 +0200

    Update EthSwap.sol

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -1,9 +1,11 @@
 pragma solidity ^0.5.0;
 
-import "./Token.sol";
+import "./Token.sol"; //5.2.when we import this SC, it´s just the code, it doesn´t tell us where it´s located on the BC.
 
 contract EthSwap {
   string public name = "EthSwap Instant Exchange";
+//5.This creates a variable that represents the token SC
+//Cuando ponemos ``Token´´ 
   Token public token;
   uint public rate = 100;
 
@@ -20,11 +22,13 @@ contract EthSwap {
     uint amount,
     uint rate
   );
-
+//5.1. We have to know where the SC is located. We need the code and the address
+//5.3.What we can do is pass the address in(Token _token)whenever we deploy the token to the network
   constructor(Token _token) public {
-    token = _token;
-  }
-
+    token = _token; //_token is just a local variable, this doesn´t get store to the BC unless we write ``token =´´
+  } //After this go to migration and write in DeploEthSwap -> token.address
+  //y tmb nos vamos a tests y en before->ethswap=await ethsawp.new ponemos token.address
+//6. REDEMPTIONR ATE = Nº of tokens they receive for 1 ether. In thsi case is 100 DAPPS
   function buyTokens() public payable {
     // Calculate the number of tokens to buy
     uint tokenAmount = msg.value * rate;
